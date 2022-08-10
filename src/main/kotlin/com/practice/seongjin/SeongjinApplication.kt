46e349f90cd7627dce73429b49d95e2c4351b56b
mobius29 <sagjin0000@naver.com>
2022-08-10 13:57:16 +0900

    complete until 3

---
+++ src/main/kotlin/com/practice/seongjin/SeongjinApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.seongjin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeongjinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeongjinApplication>(*args)
+}
---
+++ src/test/kotlin/com/practice/seongjin/SeongjinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.seongjin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeongjinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
