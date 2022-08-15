4e4d2f749bdbdf1d7563bade023cc7078fda11ec
mobius29 <sagjin0000@naver.com>
2022-08-15 13:04:38 +0900

    7. Web Layer

---
+++ src/main/kotlin/com/practice/seongjin/controller/BankController.kt
@@ -0,0 +1,15 @@
+package com.practice.seongjin.controller
+
+import com.practice.seongjin.model.Bank
+import com.practice.seongjin.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+}
